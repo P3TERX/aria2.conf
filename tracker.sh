4d9e15b85954cbdb1f78c02bfc8b36e992a9652e
P3TERX <p3terx@outlook.com>
2020-02-20 08:55:19 +0800

    Update trackers fallback URLs.

--- tracker.sh
+++ tracker.sh
@@ -4,7 +4,7 @@
 # File name：tracker.sh
 # Description: Get trackers and add to aria2.conf
 # Lisence: MIT
-# Version: 1.3
+# Version: 1.4
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
@@ -14,28 +14,30 @@ ERROR="[\033[31mERROR\033[0m]"
 ARIA2_CONF=${1:-aria2.conf}
 
 echo && echo -e "$INFO Check downloader ..."
-if [ `command -v curl` ]; then
+if [ $(command -v curl) ]; then
     DOWNLOADER='curl -fsSL'
-elif [ `command -v wget` ]; then
+elif [ $(command -v wget) ]; then
     DOWNLOADER='wget -qO-'
 else
     echo -e "$ERROR curl or wget is not installed."
 fi
 
 # BT tracker is provided by the following project.
-# https://github.com/ngosang/trackerslist
 # https://github.com/XIU2/TrackersListCollection
-# Alternative link provided by jsDelivr.
+# https://github.com/ngosang/trackerslist
+# Fallback URLs provided by jsDelivr and Cloudflare Workers
 # https://www.jsdelivr.com
+# https://workers.cloudflare.com/
 echo && echo -e "$INFO Get trackers ..."
 TRACKER=$(
-    ${DOWNLOADER} https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all_aria2.txt || \
-    ${DOWNLOADER} https://cdn.jsdelivr.net/gh/XIU2/TrackersListCollection/all_aria2.txt || \
-    {
-        ${DOWNLOADER} https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt  || \
-        ${DOWNLOADER} https://cdn.jsdelivr.net/gh/ngosang/trackerslist/trackers_all.txt
-    } \
-    | awk NF | sed ":a;N;s/\n/,/g;ta"
+    ${DOWNLOADER} https://trackerslist.com/all_aria2.txt ||
+        ${DOWNLOADER} https://cdn.jsdelivr.net/gh/XIU2/TrackersListCollection/all_aria2.txt ||
+        ${DOWNLOADER} https://trackerslist.p3terx.workers.dev/all_aria2.txt ||
+        {
+            ${DOWNLOADER} https://ngosang.github.io/trackerslist/trackers_all.txt ||
+            ${DOWNLOADER} https://cdn.jsdelivr.net/gh/ngosang/trackerslist/trackers_all.txt ||
+                ${DOWNLOADER} https://ngosang-trackerslist.p3terx.workers.dev/trackers_all.txt
+        } | awk NF | sed ":a;N;s/\n/,/g;ta"
 )
 
 [ -z $TRACKER ] && echo -e "
