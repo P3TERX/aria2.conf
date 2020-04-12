f6bfdb20aa21f74d7a9f94ac951bc6e817018031
P3TERX <p3terx@outlook.com>
2020-04-12 09:57:04 +0800

    tracker.sh: Bump version to 2.0, added the function of updating BT tracker using RPC interface.

--- tracker.sh
+++ tracker.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
-#=================================================
+#==============================================
 # https://github.com/P3TERX/aria2.conf
 # File name：tracker.sh
-# Description: Get trackers and add to aria2.conf
+# Description: Get BT trackers and add to Aria2
 # Lisence: MIT
-# Version: 1.4
+# Version: 2.0
 # Author: P3TERX
 # Blog: https://p3terx.com
-#=================================================
+#==============================================
 
-INFO="[\033[32mINFO\033[0m]"
-ERROR="[\033[31mERROR\033[0m]"
+RED_FONT_PREFIX="\033[31m"
+GREEN_FONT_PREFIX="\033[32m"
+YELLOW_FONT_PREFIX="\033[1;33m"
+LIGHT_PURPLE_FONT_PREFIX="\033[1;35m"
+FONT_COLOR_SUFFIX="\033[0m"
+INFO="[${GREEN_FONT_PREFIX}INFO${FONT_COLOR_SUFFIX}]"
+ERROR="[${RED_FONT_PREFIX}ERROR${FONT_COLOR_SUFFIX}]"
 ARIA2_CONF=${1:-aria2.conf}
-
-echo && echo -e "$INFO Check downloader ..."
-if [ $(command -v curl) ]; then
-    DOWNLOADER='curl -fsSL'
-elif [ $(command -v wget) ]; then
-    DOWNLOADER='wget -qO-'
-else
-    echo -e "$ERROR curl or wget is not installed."
-fi
 
 # BT tracker is provided by the following project.
 # https://github.com/XIU2/TrackersListCollection
@@ -28,32 +24,97 @@ fi
 # Fallback URLs provided by jsDelivr and Cloudflare Workers
 # https://www.jsdelivr.com
 # https://workers.cloudflare.com/
-echo && echo -e "$INFO Get trackers ..."
-TRACKER=$(
-    ${DOWNLOADER} https://trackerslist.com/all_aria2.txt ||
-        ${DOWNLOADER} https://cdn.jsdelivr.net/gh/XIU2/TrackersListCollection/all_aria2.txt ||
-        ${DOWNLOADER} https://trackerslist.p3terx.workers.dev/all_aria2.txt ||
-        {
-            ${DOWNLOADER} https://ngosang.github.io/trackerslist/trackers_all.txt ||
-            ${DOWNLOADER} https://cdn.jsdelivr.net/gh/ngosang/trackerslist/trackers_all.txt ||
-                ${DOWNLOADER} https://ngosang-trackerslist.p3terx.workers.dev/trackers_all.txt
-        } | awk NF | sed ":a;N;s/\n/,/g;ta"
-)
+GET_TRACKERS() {
+    echo && echo -e "$INFO Get BT trackers ..."
+    TRACKER=$(
+        curl -fsSL https://trackerslist.com/all_aria2.txt ||
+            curl -fsSL https://cdn.jsdelivr.net/gh/XIU2/TrackersListCollection/all_aria2.txt ||
+            curl -fsSL https://trackerslist.p3terx.workers.dev/all_aria2.txt ||
+            {
+                curl -fsSL https://ngosang.github.io/trackerslist/trackers_all.txt ||
+                curl -fsSL https://cdn.jsdelivr.net/gh/ngosang/trackerslist/trackers_all.txt ||
+                    curl -fsSL https://ngosang-trackerslist.p3terx.workers.dev/trackers_all.txt
+            } | awk NF | sed ":a;N;s/\n/,/g;ta"
+    )
+    [ -z $TRACKER ] && {
+        echo
+        echo -e "$ERROR Unable to get trackers, network failure or invalid links." && exit 1
+    }
+}
 
-[ -z $TRACKER ] && echo -e "
-$ERROR Unable to get trackers, network failure or invalid links." && exit 1
-echo -e "
+ECHO_TRACKERS() {
+    echo -e "
 --------------------[TRACKERS]--------------------
 ${TRACKER}
 --------------------[TRACKERS]--------------------
 "
-[ ${ARIA2_CONF} == "cat" ] && exit 0
+}
 
-echo -e "$INFO Adding trackers to '${ARIA2_CONF}' ..." && echo
-if [ ! -f ${ARIA2_CONF} ]; then
-    echo -e "$ERROR '${ARIA2_CONF}' does not exist."
+ADD_TRACKERS() {
+    echo -e "$INFO Adding BT trackers to Aria2 configuration file ${LIGHT_PURPLE_FONT_PREFIX}${ARIA2_CONF}${FONT_COLOR_SUFFIX} ..." && echo
+    if [ ! -f ${ARIA2_CONF} ]; then
+        echo -e "$ERROR '${ARIA2_CONF}' does not exist."
+        exit 1
+    else
+        [ -z $(grep "bt-tracker=" ${ARIA2_CONF}) ] && echo "bt-tracker=" >>${ARIA2_CONF}
+        sed -i "s@^\(bt-tracker=\).*@\1${TRACKER}@" ${ARIA2_CONF} && echo -e "$INFO BT trackers successfully added to Aria2 configuration file !"
+    fi
+}
+
+ADD_TRACKERS_REMOTE_RPC() {
+    echo -e "$INFO Adding BT trackers to remote Aria2: ${LIGHT_PURPLE_FONT_PREFIX}${RPC_ADDRESS}${FONT_COLOR_SUFFIX} ..." && echo
+    RPC_STATUS=$(curl "$RPC_ADDRESS/jsonrpc" -fsSd '{"jsonrpc":"2.0","method":"aria2.changeGlobalOption","id":"trackers","params":["token:'$RPC_SECRET'",{"bt-tracker":"'$TRACKER'"}]}')
+    [ $(echo $RPC_STATUS | grep OK) ] &&
+        echo -e "$INFO BT trackers successfully added to remote Aria2 !" ||
+        echo -e "$ERROR Network failure or Aria2 RPC address is incorrect."
+}
+
+ADD_TRACKERS_LOCAL_RPC() {
+    if [ ! -f ${ARIA2_CONF} ]; then
+        echo -e "$ERROR '${ARIA2_CONF}' does not exist."
+        exit 1
+    else
+        RPC_PORT=$(grep rpc-listen-port ${ARIA2_CONF} | cut -d= -f2)
+        RPC_SECRET=$(grep rpc-secret ${ARIA2_CONF} | cut -d= -f2)
+        [[ $RPC_PORT && $RPC_SECRET ]] || {
+            echo -e "$ERROR Aria2 configuration file incomplete."
+            exit 1
+        }
+        RPC_ADDRESS="localhost:$RPC_PORT"
+        echo -e "$INFO Adding BT trackers to Aria2 ..." && echo
+        RPC_STATUS=$(curl "$RPC_ADDRESS/jsonrpc" -fsSd '{"jsonrpc":"2.0","method":"aria2.changeGlobalOption","id":"trackers","params":["token:'$RPC_SECRET'",{"bt-tracker":"'$TRACKER'"}]}')
+        [ $(echo $RPC_STATUS | grep OK) ] &&
+            echo -e "$INFO BT trackers successfully added to Aria2 !" ||
+            echo -e "$ERROR Network failure or Aria2 RPC address is incorrect."
+    fi
+}
+
+[ $(command -v curl) ] || {
+    echo -e "$ERROR curl is not installed."
     exit 1
+}
+
+if [ "$1" = "cat" ]; then
+    GET_TRACKERS
+    ECHO_TRACKERS
+    exit 0
+elif [ "$1" = "RPC" ]; then
+    RPC_ADDRESS=$2
+    RPC_SECRET=$3
+    GET_TRACKERS
+    ECHO_TRACKERS
+    ADD_TRACKERS_REMOTE_RPC
+    exit 0
+elif [ "$2" = "RPC" ]; then
+    GET_TRACKERS
+    ECHO_TRACKERS
+    ADD_TRACKERS
+    echo
+    ADD_TRACKERS_LOCAL_RPC
+    exit 0
 else
-    [ -z $(grep "bt-tracker=" ${ARIA2_CONF}) ] && echo "bt-tracker=" >>${ARIA2_CONF}
-    sed -i "s@^\(bt-tracker=\).*@\1${TRACKER}@" ${ARIA2_CONF} && echo -e "$INFO Trackers added successfully!"
+    GET_TRACKERS
+    ECHO_TRACKERS
+    ADD_TRACKERS
+    exit 0
 fi
